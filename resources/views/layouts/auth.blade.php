ef287766e3c53a185e6e715afa364d70ffd5c76b
Pargev <aghabekyan.pargev@gmail.com>
2020-06-30 15:48:40 +0400

    added models

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -21,7 +21,7 @@
         <div class="lg-info-panel">
             <div class="inner-panel">
                 <div class="lg-content">
-                    <h2>Artrek Program</h2>
+                    <h2>Welcome To Lax Auto Repars Admin Panel</h2>
                 </div>
             </div>
         </div>
